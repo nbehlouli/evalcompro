792a2f17b846eb95df70910bbfe2ba9aa7a3bb7d
nabil.behlouli <nabil.behlouli@bcf5f524-3a43-4d7a-5d1a-5d39b975b2aa>
2011-05-18 22:00:41 +0000

    add update and delete action

---  evalcompro/src/administration/model/RepCompetenceModel.java
+++  evalcompro/src/administration/model/RepCompetenceModel.java
@@ -1,9 +1,12 @@
 package administration.model;
 
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.zkoss.zul.Messagebox;
 
 import administration.bean.AdministrationLoginBean;
 import administration.bean.RepCompetenceBean;
@@ -68,5 +71,170 @@ public List loadRepCompetence() throws SQLException{
 		
 		
 	}
+
+
+/**
+ * cette méthode permet d'inserer la donnée addedData dans la table structure_entreprise de la base de donnée
+ * @param addedData
+ * @return
+ * @throws ParseException 
+ */
+public boolean addRepCompBean(RepCompetenceBean addedData) throws ParseException
+{
+	
+
+	CreateDatabaseCon dbcon=new CreateDatabaseCon();
+	Connection conn=(Connection) dbcon.connectToEntrepriseDB();
+	Statement stmt;
+		
+	
+	try 
+	{
+		                                                
+		stmt = (Statement) conn.createStatement();
+		String select_structure="INSERT INTO repertoire_competence(code_famille, famille, code_groupe, groupe, code_competence, libelle_competence, definition_competence, aptitude_observable, affichable)"+
+		             		  "VALUES(#code_famille,#famille,#code_groupe,#groupe,#code_competence,#libelle_competence,#definition_competence,#aptitude_observable,#affichable)";
+		
+		select_structure = select_structure.replaceAll("#code_famille", "'"+addedData.getCode_famille()+"'");
+		select_structure = select_structure.replaceAll("#famille", "'"+addedData.getFamille()+"'");
+		select_structure = select_structure.replaceAll("#code_groupe", "'"+addedData.getCode_groupe()+"'");
+		select_structure = select_structure.replaceAll("#groupe", "'"+addedData.getGroupe()+"'");
+		select_structure = select_structure.replaceAll("#code_competence", "'"+addedData.getCode_competence()+"'");
+		select_structure = select_structure.replaceAll("#libelle_competence", "'"+addedData.getLibelle_competence()+"'");
+		select_structure = select_structure.replaceAll("#definition_competence", "'"+addedData.getDefinition_competence()+"'");
+		select_structure = select_structure.replaceAll("#aptitude_observable", "'"+addedData.getAptitude_observable()+"'");
+		select_structure = select_structure.replaceAll("#affichable", "'"+addedData.getAffichable()+"'");
+						
+	//System.out.println(select_structure);
+		
+		 stmt.execute(select_structure);
+	} 
+	catch (SQLException e) 
+	{
+		try 
+		{
+			Messagebox.show("La donnée n'a pas été insérée dans la base car il existe une donnée ayant le même code établissement", "Erreur",Messagebox.OK, Messagebox.ERROR);
+		} 
+		catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		// TODO Auto-generated catch block
+		try {
+			conn.close();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			
+			e1.printStackTrace();
+			//return false;
+		}
+		
+		
+		return false;
+	}
+	try {
+		conn.close();
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	return true;
+}
+
+public Boolean majRepCompBean(RepCompetenceBean addedData)
+{
+	
+	CreateDatabaseCon dbcon=new CreateDatabaseCon();
+	Connection conn=(Connection) dbcon.connectToEntrepriseDB();
+	Statement stmt;
+	
+	try 
+	{
+		stmt = (Statement) conn.createStatement();
+		String select_structure="UPDATE  repertoire_competence SET code_famille=#code_famille ,famille=#famille,code_groupe=#code_groupe,groupe=#groupe, code_competence=#code_competence" +
+				",libelle_competence=#libelle_competence,definition_competence=#definition_competence,aptitude_observable=#aptitude_observable,affichable=#affichable WHERE  id_repertoire_competence=# id_repertoire_competence";
+		
+		select_structure = select_structure.replaceAll("# id_repertoire_competence", Integer.toString(addedData.getId_repertoire_competence()));
+		select_structure = select_structure.replaceAll("#code_famille", "'"+addedData.getCode_famille()+"'");
+		select_structure = select_structure.replaceAll("#famille", "'"+addedData.getFamille()+"'");
+		select_structure = select_structure.replaceAll("#code_groupe", "'"+addedData.getCode_groupe()+"'");
+		select_structure = select_structure.replaceAll("#groupe", "'"+addedData.getGroupe()+"'");
+		select_structure = select_structure.replaceAll("#code_competence", "'"+addedData.getCode_competence()+"'");
+		select_structure = select_structure.replaceAll("#libelle_competence", "'"+addedData.getLibelle_competence()+"'");
+		select_structure = select_structure.replaceAll("#definition_competence", "'"+addedData.getDefinition_competence()+"'");
+		select_structure = select_structure.replaceAll("#aptitude_observable", "'"+addedData.getAptitude_observable()+"'");
+		select_structure = select_structure.replaceAll("#affichable", "'"+addedData.getAffichable()+"'");
+		
+	   //System.out.println(update_structure);
+		
+		 stmt.executeUpdate(select_structure);
+	} 
+	catch (SQLException e) 
+	{
+		try 
+		{
+			Messagebox.show("La modification n'a pas été prise en compte car il existe une donnée ayant le même code établissement", "Erreur",Messagebox.OK, Messagebox.ERROR);
+		} 
+		catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		// TODO Auto-generated catch block
+		try {
+			conn.close();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			
+			e1.printStackTrace();
+			//return false;
+		}
+		
+		
+		return false;
+	}
+	try {
+		conn.close();
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	return true;
+}
+
+/**
+ * cette classe permet de supprimer une donnée de la table structure_entreprise
+ * @param codeStructure
+ */
+public void supprimerComp(RepCompetenceBean addedData)
+{
+	CreateDatabaseCon dbcon=new CreateDatabaseCon();
+	Connection conn=(Connection) dbcon.connectToEntrepriseDB();
+	Statement stmt;
+	
+	try 
+	{
+		stmt = (Statement) conn.createStatement();
+		String sup_login="DELETE FROM  repertoire_competence  WHERE id_repertoire_competence=#id_repertoire_competence"; 
+		sup_login = sup_login.replaceAll("#id_repertoire_competence", Integer.toString(addedData.getId_repertoire_competence()));
+		
+	
+		
+		 stmt.executeUpdate(sup_login);
+	} 
+	catch (SQLException e) 
+	{
+		
+			e.printStackTrace();
+			//return false;
+
+
+	}
+	try {
+		conn.close();
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+}
 
 }
