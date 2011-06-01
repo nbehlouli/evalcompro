7fc91fa5c06a870511e7ab8a2922d70b06db242b
nabil.behlouli <nabil.behlouli@bcf5f524-3a43-4d7a-5d1a-5d39b975b2aa>
2011-06-01 19:10:32 +0000

    create formation bean

---
+++ trunk/ evalcompro/src/administration/bean/Formation.java
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+package administration.bean;
+
+/**
+ * @author FTERZI
+ *
+ */
+public class Formation
+{
+
+    private String codeFormation;
+
+    private String libelleFormation;
+
+    private String libelleDiplome;
+
+    /**
+     * 
+     */
+    public Formation()
+    {
+        // TODO Auto-generated constructor stub
+    }
+
+    public Formation( String codeFormation, String libelleFormation, String libelleDiplome )
+    {
+        super();
+        this.codeFormation = codeFormation;
+        this.libelleFormation = libelleFormation;
+        this.libelleDiplome = libelleDiplome;
+    }
+
+    public String getCodeFormation()
+    {
+        return codeFormation;
+    }
+
+    public void setCodeFormation( String codeFormation )
+    {
+        this.codeFormation = codeFormation;
+    }
+
+    public String getLibelleFormation()
+    {
+        return libelleFormation;
+    }
+
+    public void setLibelleFormation( String libelleFormation )
+    {
+        this.libelleFormation = libelleFormation;
+    }
+
+    public String getLibelleDiplome()
+    {
+        return libelleDiplome;
+    }
+
+    public void setLibelleDiplome( String libelleDiplome )
+    {
+        this.libelleDiplome = libelleDiplome;
+    }
+
+}
