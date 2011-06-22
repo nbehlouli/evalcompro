ab58696d89714bea0b53a1bc480a4ef91dcea796
nabil.behlouli <nabil.behlouli@bcf5f524-3a43-4d7a-5d1a-5d39b975b2aa>
2011-06-22 22:17:39 +0000

    compagne type bean

---
+++  evalcompro/src/administration/bean/CompagneType.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package administration.bean;
+
+import common.AbstractEnterpriseObject;
+
+/**
+ * @author FTERZI
+ *
+ */
+public class CompagneType
+    extends AbstractEnterpriseObject
+{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    private int idCompagneType;
+
+    private String libelleCompagneType;
+
+    /**
+     * 
+     */
+    public CompagneType()
+    {
+        // TODO Auto-generated constructor stub
+    }
+
+    public CompagneType( int id, String libelle )
+    {
+        this.idCompagneType = id;
+        this.libelleCompagneType = libelle;
+    }
+
+    public int getIdCompagneType()
+    {
+        return idCompagneType;
+    }
+
+    public void setIdCompagneType( int idCompagneType )
+    {
+        this.idCompagneType = idCompagneType;
+    }
+
+    public String getLibelleCOmpagneType()
+    {
+        return libelleCompagneType;
+    }
+
+    public void setLibelleCOmpagneType( String libelleCOmpagneType )
+    {
+        this.libelleCompagneType = libelleCOmpagneType;
+    }
+
+}
